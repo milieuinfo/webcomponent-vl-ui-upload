9dad05a34b6ff18c842813ce34ba49bae4b897ae
Tom Coemans <tom.coemans@gmail.com>
2020-02-10 16:05:00 +0100

    improved npm release:prepare:commit to avoid node_modules commit

--- vl-upload.src.js
+++ vl-upload.src.js
@@ -1,8 +1,8 @@
-import {VlElement, define, awaitScript, awaitUntil} from 'vl-ui-core';
+import {VlElement, define} from 'vl-ui-core';
 import 'vl-ui-icon';
-import 'vl-ui-util/dist/js/util.min.js';
-import 'vl-ui-core/dist/js/core.min.js';
-import 'vl-ui-upload/dist/upload.js';
+import '@govflanders/vl-ui-util/dist/js/util.min.js';
+import '@govflanders/vl-ui-core/dist/js/core.min.js';
+import '/dist/upload.js';
 
 /**
  * VlUpload
