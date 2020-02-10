983fb2079bcb32b25fc97db9004aa29c8f438f4a
Tom Coemans <tom.coemans@gmail.com>
2020-02-10 12:21:18 +0100

    removed unused import scripts

--- src/vl-upload.src.js
+++ src/vl-upload.src.js
@@ -1,4 +1,4 @@
-import {VlElement, define, awaitScript, awaitUntil} from '/node_modules/vl-ui-core/vl-core.js';
+import {VlElement, define} from '/node_modules/vl-ui-core/vl-core.js';
 import '/node_modules/vl-ui-icon/vl-icon.js';
 import '/node_modules/@govflanders/vl-ui-util/dist/js/util.min.js';
 import '/node_modules/@govflanders/vl-ui-core/dist/js/core.min.js';
