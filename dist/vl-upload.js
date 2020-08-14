d35ccb0de1a6ca8397b5368c1e1383c3ed95ea59
coemans <tom.coemans@gmail.com>
2020-08-14 18:11:15 +0200

    Upgrade huisstijl versie 3.11.5 (#110)

--- dist/vl-upload.js
+++ dist/vl-upload.js
@@ -10,17 +10,18 @@ import '/node_modules/vl-ui-upload/lib/upload.js';
  * @classdesc Gebruik de upload component om één of meerdere bestanden te selecteren of te slepen naar het upload veld. De gebruiker kan alternatief ook één of meerdere bestanden uploaden door op de link in het upload veld te klikken en de bestanden te selecteren in het Bestand menu.
  *
  * @extends HTMLElement
+ * @mixes vlElement
  *
- * @property {URL} url - Attribuut om de url naar waar de component moet uploaden, te definiëren.
- * @property {string} input-name - Attribuut om de key te definiëren waarmee het bestand wordt opgeladen.
- * @property {string} error-message-filesize - Attribuut om de message te definiëren wanneer er te grote bestanden zijn toegevoegd.
- * @property {string} error-message-accepted-files - Attribuut om de message te definiëren wanneer er niet-geaccepteerde bestanden zijn toegevoegd.
- * @property {string} error-message-maxfiles - Attribuut om de message te definiëren wanneer er teveel bestanden zijn toegevoegd.
- * @property {number} max-files - Attribuut om het maximaal aantal bestanden dat opgeladen mag worden, aan te duiden.
- * @property {number} max-size - Attribuut om de maximum grootte van een bestand dat opgeladen kan worden (20000000 = 2MB), aan te duiden.
- * @property {list} accepted-files - Attribuut om te op te lijsten welke bestanden worden geaccepteerd door component (extensie en mimetype).
- * @property {boolean} full-body-drop - Attribuut om te activeren of deactiveren dat het de dropzone over het heel scherm is.
- * @property {boolean} autoprocess - Attribuut om te activeren of deactiveren dat het het gedropte bestand direct moet opgeladen worden.
+ * @property {URL} data-vl-url - Attribuut om de url naar waar de component moet uploaden, te definiëren.
+ * @property {string} data-vl-input-name - Attribuut om de key te definiëren waarmee het bestand wordt opgeladen.
+ * @property {string} data-vl-error-message-filesize - Attribuut om de message te definiëren wanneer er te grote bestanden zijn toegevoegd.
+ * @property {string} data-vl-error-message-accepted-files - Attribuut om de message te definiëren wanneer er niet-geaccepteerde bestanden zijn toegevoegd.
+ * @property {string} data-vl-error-message-maxfiles - Attribuut om de message te definiëren wanneer er teveel bestanden zijn toegevoegd.
+ * @property {number} data-vl-max-files - Attribuut om het maximaal aantal bestanden dat opgeladen mag worden, aan te duiden.
+ * @property {number} data-vl-max-size - Attribuut om de maximum grootte van een bestand dat opgeladen kan worden (20000000 = 2MB), aan te duiden.
+ * @property {list} data-vl-accepted-files - Attribuut om te op te lijsten welke bestanden worden geaccepteerd door component (extensie en mimetype).
+ * @property {boolean} data-vl-full-body-drop - Attribuut om te activeren of deactiveren dat het de dropzone over het heel scherm is.
+ * @property {boolean} data-vl-autoprocess - Attribuut om te activeren of deactiveren dat het het gedropte bestand direct moet opgeladen worden.
  *
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-upload/releases/latest|Release notes}
  * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-upload/issues|Issues}
@@ -44,9 +45,9 @@ export class VlUpload extends vlElement(HTMLElement) {
   constructor() {
     super(`
       <style>
+        @import '/node_modules/vl-ui-upload/dist/style.css';
         @import '/node_modules/vl-ui-link/dist/style.css';
         @import '/node_modules/vl-ui-icon/dist/style.css';
-        @import '/node_modules/vl-ui-upload/dist/style.css';
       </style>
       <div class="vl-upload" data-vl-upload data-vl-upload-url="http://www.example.com"></div>
     `);
@@ -66,8 +67,7 @@ export class VlUpload extends vlElement(HTMLElement) {
 
   get _dropzone() {
     if (vl && vl.upload && vl.upload.dropzoneInstances) {
-      return vl.upload.dropzoneInstances.filter(
-          (dropzone) => dropzone.element === this._element)[0];
+      return vl.upload.dropzoneInstances.filter((dropzone) => dropzone.element === this._element)[0];
     }
   }
 
@@ -97,54 +97,55 @@ export class VlUpload extends vlElement(HTMLElement) {
 
   get _templates() {
     return this._template(`
-        <template id="uploadTemplate">
-          <div class="vl-upload__element">
-            <div class="vl-upload__element__label">
-              <button type="button" class="vl-upload__element__button vl-link">
-                <span is="vl-icon" data-vl-icon="paperclip"></span>
-                <span class="vl-upload__element__button__container"></span>
-              </button>
-              <small></small>
-            </div>
-          </div>
-        </template>
-    
-        <template id="previewFilesWrapper">
-          <div class="vl-upload__files">
-            <div class="vl-upload__files__container"></div>
-            <div class="vl-upload__files__input__container"></div>
-            <button class="vl-upload__files__close vl-link vl-link--icon">
-              <span is="vl-icon" data-vl-icon="trash" data-vl-link></span>
-              Verwijder alle bestanden
+      <template id="uploadTemplate">
+        <div class="vl-upload__element">
+          <div class="vl-upload__element__label">
+            <button type="button" class="vl-upload__element__button vl-link">
+              <span is="vl-icon" data-vl-icon="paperclip"></span>
+              <span class="vl-upload__element__button__container"></span>
             </button>
+            <small></small>
           </div>
-        </template>
+        </div>
+      </template>
+  
+      <template id="previewFilesWrapper">
+        <div class="vl-upload__files">
+          <div class="vl-upload__files__container"></div>
+          <div class="vl-upload__files__input__container"></div>
+          <button class="vl-upload__files__close vl-link vl-link--icon">
+            <span is="vl-icon" data-vl-icon="trash" data-vl-link></span>
+            Verwijder alle bestanden
+          </button>
+        </div>
+      </template>
 
-        <template id="previewTemplate">
-          <div class="vl-upload__file">
-            <p class="vl-upload__file__name">
-              <span is="vl-icon" class="vl-upload__file__name__icon" data-vl-icon="document"></span>
-              <span data-dz-name></span>
-              <span class="vl-upload__file__size">
-            (<span data-dz-size></span>)
-          </span>
-            </p>
-            <div class="dz-error-message">
-              <span data-dz-errormessage></span>
-            </div>
-            <button type="button" class="vl-upload__file__close vl-link vl-link--icon" data-dz-remove>
-              <span is="vl-icon" data-vl-icon="cross"></span>
-            </button>
+      <template id="previewTemplate">
+        <div class="vl-upload__file">
+          <p class="vl-upload__file__name">
+            <span is="vl-icon" class="vl-upload__file__name__icon" data-vl-icon="document"></span>
+            <span data-dz-name></span>
+            <span class="vl-upload__file__size">
+          (<span data-dz-size></span>)
+        </span>
+          </p>
+          <div class="dz-error-message">
+            <span data-dz-errormessage></span>
           </div>
-        </template>
+          <button type="button" class="vl-upload__file__close vl-link vl-link--icon" data-dz-remove>
+            <span is="vl-icon" data-vl-icon="cross"></span>
+          </button>
+        </div>
+      </template>
 
-        <template id="uploadOverlay">
-          <div class="vl-upload__overlay">
-            <p class="vl-upload__overlay__text">
-              <span is="vl-icon" data-vl-icon="paperclip" link></span>
-            </p>
-          </div>
-        </template>`);
+      <template id="uploadOverlay">
+        <div class="vl-upload__overlay">
+          <p class="vl-upload__overlay__text">
+            <span is="vl-icon" data-vl-icon="paperclip" link></span>
+          </p>
+        </div>
+      </template>
+    `);
   }
 
   get _prefix() {
