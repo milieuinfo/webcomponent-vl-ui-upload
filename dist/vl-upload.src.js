49f0cbdd5f6ef24e73bc77d85a8deab8e867f58a
bamboo <bamboo@milieuinfo.be>
2021-07-15 12:38:10 +0000

    3.7.2

--- dist/vl-upload.src.js
+++ dist/vl-upload.src.js
@@ -317,12 +317,12 @@ export class VlUpload extends vlFormValidationElement(vlElement(HTMLElement)) {
    * @param {Object} responseBody - body van de response bij het opladen van het bestand
    * @return {void}
    */
-  addFile({ name, size, id, responseBody }) {
+  addFile({ name, size, id, type, responseBody }) {
     const autoprocessActive = this.dataset.vlAutoprocess != undefined;
     if (autoprocessActive) {
       this._disableAutoProcessQueue();
     }
-    const file = { name: name, size: size, id: id, responseBody: responseBody };
+    const file = { name: name, size: size, id: id, type: type, responseBody: responseBody };
     this._dropzone.addFile(file);
     this._dropzone.emit('complete', file);
     file.status = 'success';
